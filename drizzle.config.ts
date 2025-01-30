ef0f6225431cebad9a2acc7da691dd07f00ae6fd
Tashrik Anam <anampartho@users.noreply.github.com>
2025-01-31 01:26:32 +0600

    feat: update docker setup

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from 'drizzle-kit';
-if (!process.env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
+if (!process.env.DB_URL) throw new Error('DB_URL is not set');
 
 export default defineConfig({
 	schema: './src/lib/server/db/schema.ts',
 	out: './drizzle',
 	dbCredentials: {
-		url: process.env.DATABASE_URL
+		url: process.env.DB_URL
 	},
 	verbose: true,
 	strict: true,
