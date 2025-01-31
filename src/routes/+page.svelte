e41da3c46a138e3017c046d1c57ca8130357daa9
Tashrik Anam <anampartho@users.noreply.github.com>
2025-02-01 04:35:45 +0600

    feat: basic ui - Add icons - Add fonts - Add basic cards - Add route

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,22 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import Button from '$lib/components/ui/Button.svelte';
+	import YoutubeIcon from '$lib/icons/youtube.svelte';
+</script>
+
+<div class="card flex justify-center">
+	<div
+		class="card card-hover block max-w-md divide-y overflow-hidden border-[1px] border-surface-200-800 divide-surface-200-800 preset-filled-surface-100-900"
+	>
+		<div class="space-y-4 p-4">
+			<div>
+				<h3 class="h3 text-center">Welcome to Listd!</h3>
+			</div>
+			<p class="text-center opacity-60">
+				A place where you can create and share your favorite feeds from Youtube and other platforms.
+			</p>
+		</div>
+		<footer class="flex items-center justify-center gap-4 p-4">
+			<Button>Login using Youtube</Button>
+		</footer>
+	</div>
+</div>
