2899013542e563ab0dcbb9c97ce0017deae8dcd4
Tashrik Anam <anampartho@users.noreply.github.com>
2025-02-01 03:36:45 +0600

    feat: add app header

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,16 @@
 <script lang="ts">
 	import '../app.css';
+	import AppHeader from '$lib/components/AppHeader.svelte';
+
 	let { children } = $props();
 </script>
 
-{@render children()}
+<div class="grid h-screen grid-rows-[auto_1fr_auto]">
+	<!-- Header -->
+	<AppHeader />
+
+	<!-- Main -->
+	<main class="space-y-4 p-4">
+		{@render children()}
+	</main>
+</div>
