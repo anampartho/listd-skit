84ae9f154ba947638e09fefb8ca8528ba6b4ce1f
Tashrik Anam <anampartho@users.noreply.github.com>
2025-02-01 03:36:15 +0600

    feat: setup daisy ui (#4)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,12 +3,24 @@ import forms from '@tailwindcss/forms';
 import typography from '@tailwindcss/typography';
 import type { Config } from 'tailwindcss';
 
+import { skeleton, contentPath } from '@skeletonlabs/skeleton/plugin';
+import * as themes from '@skeletonlabs/skeleton/themes';
+
 export default {
-	content: ['./src/**/*.{html,js,svelte,ts}'],
+	darkMode: 'selector',
+
+	content: ['./src/**/*.{html,js,svelte,ts}', contentPath(import.meta.url, 'svelte')],
 
 	theme: {
 		extend: {}
 	},
 
-	plugins: [typography, forms, containerQueries]
+	plugins: [
+		typography,
+		forms,
+		containerQueries,
+		skeleton({
+			themes: [themes.catppuccin]
+		})
+	]
 } satisfies Config;
