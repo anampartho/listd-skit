147c8bd3fe7512ab15dc28578dd75b05184b0dbd
Tashrik Anam <anampartho@users.noreply.github.com>
2025-02-01 04:37:37 +0600

    feat: add basic UI (#5)
    * feat: add app header
    
    * feat: basic ui
    - Add icons
    - Add fonts
    - Add basic cards
    - Add route
    
    * fix: remove wrong class

---
+++ src/lib/components/AppHeader.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { AppBar } from '@skeletonlabs/skeleton-svelte';
+	import Icon from '@iconify/svelte';
+</script>
+
+<AppBar>
+	{#snippet lead()}
+		<h1 class="mb-0 flex items-center font-extrabold">LISTD</h1>
+	{/snippet}
+	{#snippet trail()}
+		<button><Icon icon="lucide:sun-moon" width={24} height={24} /></button>
+	{/snippet}
+</AppBar>
---
+++ src/lib/components/ui/Button.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+	let { children } = $props();
+</script>
+
+<a class="btn font-bold preset-filled-secondary-500" href="/login">{@render children()}</a>
